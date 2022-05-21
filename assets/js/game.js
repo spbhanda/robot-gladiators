b903637b3b9367163781d0899716ad28461c69b5
spbhanda <spbhanda@gmail.com>
2022-05-21 10:17:43 -0500

    add playerName variable

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,1 +1,15 @@
-window.alert("Javascript is running");
+// creates a function named "fight"
+var playerName = window.prompt("What is your robot's name?");
+console.log(playerName);
+console.log(10+10);
+console.log("Our robot's name is " + playerName);
+
+var name = "Shom";
+console.log(name);
+
+function fight() {
+  window.alert("The fight has begun!");
+}
+// fight();
+
+
